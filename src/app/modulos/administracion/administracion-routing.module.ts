ce63c01d14d39c0ee78cf736cdd67d97cf76a550
alejos17 <alejos17@gmail.com>
2021-11-26 10:32:24 -0500

    Formulario-CrearPersonayVeterinaria

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { CrearVeterinariaComponent } from './veterinarias/crear-veterinaria/crear-veterinaria.component';
 
 const routes: Routes = [
   {
     path: 'crear-persona',
     component: CrearPersonaComponent
+  },
+  {
+    path: 'crear-veterinaria',
+    component: CrearVeterinariaComponent
   },
 ];
 
