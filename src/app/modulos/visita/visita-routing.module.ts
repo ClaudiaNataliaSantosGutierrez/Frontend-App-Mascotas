f67b172938875e147933f024131338e57f0c3fc2
DanielSarmiento04 <josedanielsarmineto@gmail.com>
2021-11-28 19:43:02 -0500

    Integracion visita en modulo principal

--- src/app/modulos/visita/visita-routing.module.ts
+++ src/app/modulos/visita/visita-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HacerVisitaComponent } from './hacer-visita/hacer-visita.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hacer-visita',
+    component : HacerVisitaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
